6f4a6694093fe29c12b9e07bf70c9f864b47d4d9
Caglar Kilic <kilicalicaglar@gmail.com>
2025-07-14 23:43:09 +0300

    Added coverage exclusions to config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
+    coverage: {
+      include: ['src/app.ts', 'src/card.ts', 'src/form.ts', 'src/table.ts']
+    }
   },
 })
